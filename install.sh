c05c1c7380bfb99e4a8c605e9d199f1e8833f5c3
FarmerLP <farmer.lp@gmx.at>
2025-03-03 10:01:11 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ if [ "$EUID" -eq 0 ];then
   apt install tesseract-ocr
   apt install tesseract-ocr-deu
   apt install pandoc
+  sudo apt install texlive-latex-recommended
+  sudo apt install texlive-latex-base
 else
   echo "Please run as root"
   exit 42
